0cf9a91afdf77d66587ee85d9ddbe83a429d7b73
ziguirifow <thulio.alves@gmail.com>
2020-03-06 23:28:54 -0300

    Add files via upload

---
+++ js/calcula-imc.js
@@ -0,0 +1,63 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente")
+
+for (var i = 0; i < pacientes.length; i++) {
+
+	var paciente = pacientes[i];
+
+	var tdPeso = paciente.querySelector(".info-peso");
+	var peso = tdPeso.textContent;
+
+	var tdAltura = paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
+
+	var tdImc = paciente.querySelector(".info-imc");
+
+	var pesoValido = validaPeso(peso);
+	var alturaValida = validaAltura(altura);
+
+	if (!pesoValido) {
+		console.log("Peso inválido");
+		pesoValido = false;
+		tdImc.textContent = "Peso inválido!";
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if (!alturaValida) {
+		console.log("Altura inválida");
+		alturaValida = false;
+		tdImc.textContent = "Altura inválida!";
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if (alturaValida && pesoValido) {
+		var imc = calculaImc(peso, altura);
+		tdImc.textContent = imc;
+	}
+}
+
+function validaPeso(peso) {
+	if (peso >= 0 && peso < 1000) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function validaAltura(altura) {
+	if (altura >= 0 && altura < 3.0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function calculaImc(peso, altura) {
+	var imc = 0;
+
+	imc = peso / (altura * altura);
+
+	return imc.toFixed(2);
+}
